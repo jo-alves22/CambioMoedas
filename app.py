8afa4796e7bae29881cb0757b16a18b3e9f60280
jo-alves22 <jonasadrianoalves@gmail.com>
2023-05-12 21:26:23 -0300

    Alteracao nos parametros para buscar outras moedas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
@@ -8,11 +8,12 @@ def index():
     response = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL")
     data = response.json()
     print(data)
-    print(data['USDBRL']['high'])
+    print(data['USDBRL']['ask'])
     cifra = 'R$'
-    dolarvalor = data['USDBRL']['high']
-    return render_template("index.html", dolar=dolarvalor, cifra=cifra)
+    valormoeda = data['USDBRL']['ask']
+    return render_template("index.html", valormoeda=valormoeda, cifra=cifra)
 
+#USD-BRL,EUR-BRL,BTC-BRL,EUR-USD,BTC-USD,AUD-BRL,CAD-BRL,ARS-BRL,BRL-ARS,CHF-BRL,GBP-BRL
 #https://docs.awesomeapi.com.br/api-de-moedas
 
 #response = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL')
@@ -21,6 +22,22 @@ def index():
 
 #response = requests.get('https://economia.awesomeapi.com.br/json/daily/USD-BRL/15')
 #print(response.text)
+
+
+@app.route('/buscarcotacao', methods=['POST'])
+def buscarcotacao():
+    moeda1 = str(request.form.get('moeda1'))
+    moeda2 = str(request.form.get('moeda2'))
+    response = requests.get(f"https://economia.awesomeapi.com.br/last/{moeda1}-{moeda2}")
+    valor = response.json()
+    for item, value in valor.items():
+        if 'ask' in value:
+            print(value['ask'])
+            valormoeda = value['ask']
+    #print(valor['EURBRL']['ask'])
+    cifra = 'R$'
+    return render_template("index.html", valormoeda=valormoeda, cifra=cifra)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
